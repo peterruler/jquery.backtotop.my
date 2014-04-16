e8b662c98585b110362751788e9713aa6ee72c6e
peterruler <7starch@gmail.com>
2014-04-16 19:47:23 +0200

    added btn display and fadeIn fadeOut

--- src/jquery.backtotop.js
+++ src/jquery.backtotop.js
@@ -15,9 +15,10 @@
 ;(function ( $, window, document, undefined ) {
 
     // Create the defaults once
-    var pluginName = "jQScrollTop",
+    var pluginName = "jQScrollTop", instance = null, that, logcount,
         defaults = {
-            timeout: 2000
+            timeout: 2000,
+            DEBUG :false
         };
     // The actual plugin constructor
     function Plugin( element, options ) {
@@ -28,30 +29,80 @@
         this._defaults = defaults;
         this._name = pluginName;
 
+        that = this;
+        $.data(that, "logcount",1);
+        this.instance = null;
         this.init();
     }
 
     Plugin.prototype.init = function () {
-
+        Plugin.prototype.getInstance();
+        if(document.body.scrollTop < window.innerHeight) {
+            $("#upBtn button").hide();
+        }
         return true;
     };
+    Plugin.prototype.getInstance = function() {
+        if(this.instance == null) {
+            this.instance = addButton();
+            fadeInBtn();
+        }
+        return this.instance;
+    };
     var addButton = function() {
-        var upBtn = "<span id='up-btn'></span>";
+        var upBtn = '<a id="upBtn" href="javascript:;"> <button class="glyphicon glyphicon-arrow-up"></button></a>';
         $('body').append(upBtn);
+        return upBtn;
     };
-    var removeBtn = function() {
-      $('#up-btn').remove();
+    var fadeInBtn = function() {
+        $("#upBtn button").hide();
+        $("#upBtn button").fadeIn("slow");
     };
-    $('#up-btn').on('click', function() {
-        var inWinPosition= $(window).screenX;
-        var timeout = (2/this.o.timeout)*inWinPosition;
-        $("html, body").animate({ scrollTop: -1* inWinPosition }, timeout, "swing");
+    Plugin.prototype.removeBtn = function() {
+        $("#upBtn button").animate({"opactity":0}, 5000).remove();
+        if(this.instance != null) {
+            this.instance = null;
+        }
+    };
+    var doc = document;
+
+    var ycoord = Math.max(
+        doc.documentElement.scrollTop,
+        doc.body.scrollTop
+    );
+
+    var xcoord = 0;
+
+    $("#upBtn").on("click", function(e) {
+        e.preventDefault()
+        var completeCalled = false;
+        $("html, body").animate({ scrollTop: ycoord });
+        Plugin.prototype.scrollTopOfPage();
     });
+    Plugin.prototype.scrollTopOfPage = function() {
+
+        var ycoord = Math.max(
+            document.documentElement.scrollTop,
+            document.body.scrollTop
+        );
+        $("body").animate({ scrollTop: 0 });
+    };
+    var log = function(msg, txt) {
+        var logcount = $.data(that, "logcount");
+        var separator = "";
+        for (var n = 0; n < logcount; n++) {
+            separator += "-";
+        }
+    console.log(separator+" "+logcount+". "+msg+" ="+ txt);
+          logcount++;
+        $.data(that, "logcount",logcount);
+    };
     $(window).on('scroll',function(){
-        if(this.scrollTop > this.clientHeight) {
-            addButton();
+        (that.o.DEBUG) ? log("document.body.scrollTop",document.body.scrollTop) : true === true;
+        if(document.body.scrollTop > window.innerHeight) {
+            Plugin.prototype.getInstance();
         } else {
-            removeBtn();
+            Plugin.prototype.removeBtn();
         }
     });
     $.fn.jQScrollTop = function ( options ) {
